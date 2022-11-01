8649f97786c0752e02a10e2fc400cd1824602a04
Ziran94 <Webcaliber.ufa@gmail.com>
2022-11-01 16:52:33 +0500

    Модели для регионов

---
+++ frontend/models/Regions.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "regions".
+ *
+ * @property int $id
+ * @property string $code
+ * @property string $name
+ * @property string $postIndex
+ * @property int $status
+ *
+ * @property Contacts[] $contacts
+ */
+class Regions extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'regions';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'name'], 'required'],
+            [['status'], 'integer'],
+            [['code', 'postIndex'], 'string', 'max' => 20],
+            [['name'], 'string', 'max' => 255],
+            [['code'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'code' => 'Code',
+            'name' => 'Name',
+            'postIndex' => 'Post Index',
+            'status' => 'Status',
+        ];
+    }
+
+    /**
+     * Gets query for [[Contacts]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getContacts()
+    {
+        return $this->hasMany(Contacts::class, ['idRegion' => 'id']);
+    }
+
+    public function toggleStatus()
+    {
+        if ($this->status == 0)
+            $this->status = 1;
+        else{
+            $this->status = 0;
+        }
+        $this->save();
+    }
+}
